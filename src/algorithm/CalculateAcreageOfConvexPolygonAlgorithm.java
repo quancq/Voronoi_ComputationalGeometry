36e46cfbdaad2acb06c0e4072554193c931c9bf4
quancq <quancq.it@gmail.com>
2018-04-17 18:15:30 +0700

    add some algorithm files

---
+++ src/algorithm/CalculateAcreageOfConvexPolygonAlgorithm.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithm;
+
+import datastructure.voronoi_diagram.Point;
+import java.util.List;
+
+/**
+ *
+ * @author quancq
+ */
+public class CalculateAcreageOfConvexPolygonAlgorithm {
+ 
+    public static double triangulationDivideAndConquer(List<? extends Point> listVertices){
+        System.out.println("\nRunning " + AlgorithmManager.Triangulation_DAC + "\n");
+        double acreage = 0;
+        
+        return acreage;
+    }
+}
