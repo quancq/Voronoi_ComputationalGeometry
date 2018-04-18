c5771d3f30570c5c4377ef7e06f9819919ba84e4
quancq <quancq.it@gmail.com>
2018-04-19 00:57:17 +0700

    add field to abstract class Event

--- src/datastructure/event/Event.java
+++ src/datastructure/event/Event.java
@@ -5,10 +5,30 @@
  */
 package datastructure.event;
 
+import datastructure.beachline.BeachLine;
+import datastructure.voronoi_diagram.Point;
+import datastructure.voronoi_diagram.VoronoiDiagram;
+import java.util.Collection;
+
 /**
  *
  * @author quancq
  */
 public abstract class Event {
+    private final Point point;
+
+    public Event(Point point) {
+        this.point = point;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" + "point=" + point + '}';
+    }
     
+    public abstract void handleEvent(Collection<Event> eventQueue, BeachLine beachLine, VoronoiDiagram voronoiDiagram);
 }
