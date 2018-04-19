3224d7e9f1c2d450eae3326e9f05ed67f889571f
quancq <quancq.it@gmail.com>
2018-04-19 23:59:59 +0700

    test read/write file Voronoi diagram

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,10 +5,27 @@
  */
 package main;
 
+import datastructure.voronoi_diagram.VoronoiDiagram;
+import java.io.File;
+import java.nio.file.Paths;
+import util.DataIO;
+import util.UtilManager;
+
 /**
  *
  * @author quancq
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+        int numSite = 10;
+        int numEdge = 15;
+        double maxX = 30.5;
+        double maxY = 50;
+
+        VoronoiDiagram VD = UtilManager.randomVD(numSite, numEdge, maxX, maxY);
+        File file = new File("src/dataset/input_site-" + numSite + "-edge-" + numEdge + ".txt");
+
+        DataIO.writeVoronoiDiagram(file.getAbsolutePath(), VD);
+    }
 }
