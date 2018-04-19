8f3d265894da3fcf357258b4cf75cd659660f327
quancq <quancq.it@gmail.com>
2018-04-20 00:11:25 +0700

    add method to generate random Voronoi diagram

--- src/util/UtilManager.java
+++ src/util/UtilManager.java
@@ -5,9 +5,15 @@
  */
 package util;
 
+import datastructure.voronoi_diagram.HalfEdge;
 import datastructure.voronoi_diagram.Point;
+import datastructure.voronoi_diagram.Vertex;
+import datastructure.voronoi_diagram.VoronoiDiagram;
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
 
 /**
  * Class implement static utility method
@@ -17,7 +23,7 @@ import static java.lang.Math.sqrt;
 public class UtilManager {
 
     /**
-     * 
+     *
      * @param p1
      * @param p2
      * @return Euclidean distance between p1 and p2
@@ -27,16 +33,17 @@ public class UtilManager {
     }
 
     /**
-     * 
+     *
      * @param x
-     * @return square of x. This utility method order to code quickly and briefly
+     * @return square of x. This utility method order to code quickly and
+     * briefly
      */
     public static double sq(double x) {
         return pow(x, 2);
     }
 
     /**
-     * 
+     *
      * @param p1 is first point
      * @param p2 is second point
      * @param p3 is third point
@@ -49,24 +56,55 @@ public class UtilManager {
         double b2 = 2 * (p3.y - p1.y);
         double c1 = (p2.x - p1.x) * (p2.x + p1.x) + (p2.y - p1.y) * (p2.y + p1.y);
         double c2 = (p3.x - p1.x) * (p3.x + p1.x) + (p3.y - p1.y) * (p3.y + p1.y);
-        
+
         // solve two equations
         // equation1: a1 * x + b1 * y = c1
         // equation2: a2 * x + b2 * y = c2
-        
         double D = a1 * b2 - a2 * b1;
-        if(D == 0){
+        if (D == 0) {
             // no solution or an infinite number of solutions
             return null;
         }
         // exsit only 1 solution
         double Dx = c1 * b2 - c2 * b1;
         double Dy = a1 * c2 - a2 * c1;
-        
+
         double x = Dx / D;
         double y = Dy / D;
         Point center = new Point(x, y);
 
         return center;
+    }
+
+    public static VoronoiDiagram randomVD(int numSite, int numEdge, double maxX, double maxY) {
+        Random R = new Random();
+        VoronoiDiagram VD = new VoronoiDiagram();
+        ArrayList<Point> siteList = new ArrayList<>();
+        HashSet<HalfEdge> hs = new HashSet<>();
+        
+        for(int i = 0; i < numSite; ++i){
+            siteList.add(randomPoint(maxX, maxY));
+        }
+        
+        for(int i = 0; i < numEdge; ++i){
+            HalfEdge halfEdge1 = new HalfEdge(new Vertex(randomPoint(maxX, maxY)));
+            HalfEdge halfEdge2 = new HalfEdge(new Vertex(randomPoint(maxX, maxY)));
+            halfEdge1.setTwinEdge(halfEdge2);
+            halfEdge2.setTwinEdge(halfEdge1);
+            
+            hs.add(halfEdge1);
+            hs.add(halfEdge2);
+        }
+        
+        VD.setHsHalfEdges(hs);
+        VD.setListSites(siteList);
+        return VD;
+    }
+    
+    public static Point randomPoint(double maxX, double maxY){
+        Random R = new Random();
+        double x = R.nextDouble() * maxX;
+        double y = R.nextDouble() * maxY;
+        return new Point(x, y);
     }
 }
