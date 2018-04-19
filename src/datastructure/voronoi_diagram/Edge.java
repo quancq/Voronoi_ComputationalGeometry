d17425f2e1b6c56141e2934b087d8d6f1974cc53
quancq <quancq.it@gmail.com>
2018-04-19 23:58:52 +0700

    add new class

---
+++ src/datastructure/voronoi_diagram/Edge.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructure.voronoi_diagram;
+
+/**
+ *
+ * @author quancq
+ */
+public class Edge {
+
+    private Point v1;
+    private Point v2;
+
+    public Edge(Point v1, Point v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public Point getV1() {
+        return v1;
+    }
+
+    public Point getV2() {
+        return v2;
+    }
+
+    public void setV1(Point v1) {
+        this.v1 = v1;
+    }
+
+    public void setV2(Point v2) {
+        this.v2 = v2;
+    }
+
+}
