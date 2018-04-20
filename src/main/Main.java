804c3eea882f99041c53ef9e4b2de02fb59a70a4
quancq <quancq.it@gmail.com>
2018-04-20 11:51:51 +0700

    test read/write file

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,17 +19,31 @@ public class Main {
 
     public static void main(String[] args) {
         int numSite = 10;
-        int numEdge = 15;
+        int numEdge = 18;
         double maxX = 30.5;
         double maxY = 50;
 
-        VoronoiDiagram VD = UtilManager.randomVD(numSite, numEdge, maxX, maxY);
-        String path = "src/dataset/input_site-" + numSite + "-edge-" + numEdge + ".txt";
-        File file = new File(path);
+        VoronoiDiagram VD1 = UtilManager.randomVD(numSite, numEdge, maxX, maxY);
+        System.out.println(" vd1\n " + VD1.getHsHalfEdges());
+        String path1 = "src/dataset/input_site-" + numSite + "-edge-" + numEdge + ".txt";
+        String path2 = "src/dataset/input_site-" + numSite + "-edge-" + numEdge + ".inp";
+        File file1 = new File(path1);
+        File file2 = new File(path2);
 
-        DataIO.writeVoronoiDiagram(file.getAbsolutePath(), VD);
+        // write text to file 1
+//        DataIO.writeVoronoiDiagram(file1.getAbsolutePath(), VD1);
         
-        VoronoiDiagram vd2 = DataIO.readVornoiDiagram(file.getAbsolutePath());
-        DataIO.writeVoronoiDiagram(file.getAbsolutePath(), vd2);
+        // read text from file 1
+//        VoronoiDiagram VD2 = DataIO.readVoronoiDiagram(file1.getAbsolutePath());
+        
+        // write oject to file 2
+        DataIO.writeVoronoiDiagramObject(file2.getAbsolutePath(), VD1);
+        
+        // read object from file 2
+        VoronoiDiagram VD3 = DataIO.readVoronoiDiagramObject(file2.getAbsolutePath());
+        
+        // print
+        System.out.println(VD3);
+        System.out.println(VD3.getHsHalfEdges());
     }
 }
