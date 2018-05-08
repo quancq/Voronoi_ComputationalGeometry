deda37b0c8273fd431c6831ad26843579b674cd1
quancq <quancq.it@gmail.com>
2018-05-08 18:38:48 +0700

    add main method to MainGui.java

--- src/gui/MainGui.java
+++ src/gui/MainGui.java
@@ -8,13 +8,49 @@ package gui;
 import datastructure.voronoi_diagram.Edge;
 import datastructure.voronoi_diagram.Point;
 import datastructure.voronoi_diagram.VoronoiDiagram;
+import java.io.File;
 import java.util.ArrayList;
+import util.DataIO;
+import util.UtilManager;
 
 /**
  *
  * @author quancq
  */
 public class MainGui {
+    
+    public static void main(String[] args) {
+        int numSite = 10;
+        int numEdge = 18;
+        double maxX = 30.5;
+        double maxY = 50;
+
+        VoronoiDiagram VD1 = UtilManager.randomVD(numSite, numEdge, maxX, maxY);
+        System.out.println(" vd1\n " + VD1.getHsHalfEdges());
+        String path1 = "src/dataset/input_site-" + numSite + "-edge-" + numEdge + ".txt";
+        String path2 = "src/dataset/input_site-" + numSite + "-edge-" + numEdge + ".inp";
+        File file1 = new File(path1);
+        File file2 = new File(path2);
+
+        // write text to file 1
+//        DataIO.writeVoronoiDiagram(file1.getAbsolutePath(), VD1);
+        
+        // read text from file 1
+//        VoronoiDiagram VD2 = DataIO.readVoronoiDiagram(file1.getAbsolutePath());
+        
+        // write oject to file 2
+        DataIO.writeVoronoiDiagramObject(file2.getAbsolutePath(), VD1);
+        
+        // read object from file 2
+        VoronoiDiagram VD3 = DataIO.readVoronoiDiagramObject(file2.getAbsolutePath());
+        
+        // print
+        System.out.println(VD3);
+        System.out.println(VD3.getHsHalfEdges());
+        
+        MainGui.showVoronoiDiagram(VD3);
+    }
+    
     public static void showVoronoiDiagram(VoronoiDiagram VD){
         System.out.println("\n============= Show GUI =============");
         ArrayList<Point> siteList = VD.getListSites();
